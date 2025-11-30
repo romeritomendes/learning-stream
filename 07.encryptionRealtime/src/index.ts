07f3cda744a6c457c8d8a392e2ff7ff6161c6ad0
Romerito Silva <Ext-Romerito.Silva@hitachiastemo.com>
2025-11-30 21:52:22 +0000

    fixed but not the best solution

--- 07.encryptionRealtime/src/index.ts
+++ 07.encryptionRealtime/src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import multer from 'multer';
 import fs from 'node:fs';
 import { Encryptor } from './BO/cripter.js';
+import { Transform } from 'node:stream';
 
 const app = express();
 const upload = multer({
@@ -41,20 +42,50 @@ app.post('/encrypt', upload.single('secret'), (req, res) => {
 app.post('/decrypt', upload.single('secret'), (req, res) => {
     if(!req.file) return res.status(400).send('Secret file is required!');
     
-    const fileStream = fs.createReadStream(req.file.path);
-    // const destStream = fs.createWriteStream(`./dest/${req.file.filename}.dec`);
-
-    res.writeHead(200, {
-        "content-type": "application/octet-stream",
-        "content-disposition": `attachment; filename="${req.file.originalname}.dec"`
-    });
-    fileStream.pipe(cripter.decrypt(req.body.encMessage)).pipe(res);
-
     res.on('finish', () => {
         fs.unlink((req.file?.path as string), err => {
             if(err) console.warn('Temp cleanup failed:', err);
-        })
-    })
+        });
+        console.log('FIM REAL')
+    });
+
+    const fileStream = fs.createReadStream(req.file.path);
+    const transform = cripter.decrypt(req.body.encMessage);
+
+    transform.on('error', err => {
+        console.error('Decrypt error:', err);
+        fileStream.destroy();
+        res.status(400).send('Decrypt error: wrong password!');
+    });
+
+    fileStream.on('data', chunk => {
+        const testTransform = cripter.decrypt(req.body.encMessage);
+        testTransform.on('error', err => {
+            console.error(`Cryptor Error:`, err);
+            fileStream.destroy(err);
+            testTransform.destroy(err);
+            res.status(400).send('Decrypt error: wrong password!');
+        });
+
+        let hasChunk = 0;
+        testTransform.on('data', () => {
+            if(!res.headersSent && hasChunk === 1) {
+                console.log('Write HEADER Download!');
+                res.writeHead(200, {
+                    'Content-Type': 'application/octet-stream',
+                    'Content-Disposition': `attachment; filename="${req.file?.originalname?.replace('.enc', '.dec')}"`
+                });
+                transform.write(chunk);
+                fileStream.pipe(transform).pipe(res);
+            }
+            hasChunk++;
+        });
+
+        testTransform.write(chunk);
+        testTransform.end();
+    });
+
+    console.log('FIM')
 });
 
 app.listen(PORT, ()=> {
